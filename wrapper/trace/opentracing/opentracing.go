5e7ce3c85ec3f02ec2cb9873cd8fa7489263c6cc
Bastian Winkler <buz@netbuz.org>
2018-04-04 17:13:10 +0200

    opentracing: Add additional opentracing tags
    This commits add the following tags to the opentracing wrappers:
     - span.kind: "client" or "server" for RPC, "producer" or "consumer"
       for pubsub.
     - message_bus.destination: Is set to the topic for pubsub.
     - error: Is set to true if an error happened.
    
    In case of errors, the "event" and "message" log fields are set as well.

--- wrapper/trace/opentracing/opentracing.go
+++ wrapper/trace/opentracing/opentracing.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 
 	"context"
+
 	"github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/metadata"
 	"github.com/micro/go-micro/server"
-	"github.com/opentracing/opentracing-go"
+	opentracing "github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	"github.com/opentracing/opentracing-go/log"
 )
 
 type otWrapper struct {
@@ -16,7 +19,7 @@ type otWrapper struct {
 	client.Client
 }
 
-func traceIntoContext(ctx context.Context, tracer opentracing.Tracer, name string) (context.Context, opentracing.Span, error) {
+func traceIntoContext(ctx context.Context, tracer opentracing.Tracer, name string, tag opentracing.Tag) (context.Context, opentracing.Span, error) {
 	md, ok := metadata.FromContext(ctx)
 	if !ok {
 		md = make(map[string]string)
@@ -24,9 +27,9 @@ func traceIntoContext(ctx context.Context, tracer opentracing.Tracer, name strin
 	var sp opentracing.Span
 	wireContext, err := tracer.Extract(opentracing.TextMap, opentracing.TextMapCarrier(md))
 	if err != nil {
-		sp = tracer.StartSpan(name)
+		sp = tracer.StartSpan(name, tag)
 	} else {
-		sp = tracer.StartSpan(name, opentracing.ChildOf(wireContext))
+		sp = tracer.StartSpan(name, opentracing.ChildOf(wireContext), tag)
 	}
 	if err := sp.Tracer().Inject(sp.Context(), opentracing.TextMap, opentracing.TextMapCarrier(md)); err != nil {
 		return nil, nil, err
@@ -38,22 +41,23 @@ func traceIntoContext(ctx context.Context, tracer opentracing.Tracer, name strin
 
 func (o *otWrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
 	name := fmt.Sprintf("%s.%s", req.Service(), req.Method())
-	ctx, span, err := traceIntoContext(ctx, o.ot, name)
+	ctx, span, err := traceIntoContext(ctx, o.ot, name, ext.SpanKindRPCClient)
 	if err != nil {
 		return err
 	}
-	defer span.Finish()
-	return o.Client.Call(ctx, req, rsp, opts...)
+	err = o.Client.Call(ctx, req, rsp, opts...)
+	return finishSpan(span, err)
 }
 
 func (o *otWrapper) Publish(ctx context.Context, p client.Publication, opts ...client.PublishOption) error {
 	name := fmt.Sprintf("Pub to %s", p.Topic())
-	ctx, span, err := traceIntoContext(ctx, o.ot, name)
+	ctx, span, err := traceIntoContext(ctx, o.ot, name, ext.SpanKindProducer)
 	if err != nil {
 		return err
 	}
-	defer span.Finish()
-	return o.Client.Publish(ctx, p, opts...)
+	ext.MessageBusDestination.Set(span, p.Topic())
+	err = o.Client.Publish(ctx, p, opts...)
+	return finishSpan(span, err)
 }
 
 // NewClientWrapper accepts an open tracing Trace and returns a Client Wrapper
@@ -68,12 +72,12 @@ func NewHandlerWrapper(ot opentracing.Tracer) server.HandlerWrapper {
 	return func(h server.HandlerFunc) server.HandlerFunc {
 		return func(ctx context.Context, req server.Request, rsp interface{}) error {
 			name := fmt.Sprintf("%s.%s", req.Service(), req.Method())
-			ctx, span, err := traceIntoContext(ctx, ot, name)
+			ctx, span, err := traceIntoContext(ctx, ot, name, ext.SpanKindRPCServer)
 			if err != nil {
 				return err
 			}
-			defer span.Finish()
-			return h(ctx, req, rsp)
+			err = h(ctx, req, rsp)
+			return finishSpan(span, err)
 		}
 	}
 }
@@ -83,12 +87,21 @@ func NewSubscriberWrapper(ot opentracing.Tracer) server.SubscriberWrapper {
 	return func(next server.SubscriberFunc) server.SubscriberFunc {
 		return func(ctx context.Context, msg server.Publication) error {
 			name := "Pub to " + msg.Topic()
-			ctx, span, err := traceIntoContext(ctx, ot, name)
+			ctx, span, err := traceIntoContext(ctx, ot, name, ext.SpanKindConsumer)
 			if err != nil {
 				return err
 			}
-			defer span.Finish()
-			return next(ctx, msg)
+			err = next(ctx, msg)
+			return finishSpan(span, err)
 		}
 	}
+}
+
+func finishSpan(span opentracing.Span, err error) error {
+	if err != nil {
+		ext.Error.Set(span, true)
+		span.LogFields(log.String("event", "error"), log.String("message", err.Error()))
+	}
+	span.Finish()
+	return err
 }
